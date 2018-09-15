855c2f79c1fb8c38689c5e1b242d046c3377401f
kevan <kevan.tan@live.com>
2018-09-15 02:37:49 -0400

    edited trafficMap code and added parking-citation.json

--- src/main/webapp/trafficMap.js
+++ src/main/webapp/trafficMap.js
@@ -15,30 +15,28 @@ function loadJSON(callback) {
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 2,
-	  center: new google.maps.LatLng(2.8,-187.3),
+	  zoom: 12,
+	  center: new google.maps.LatLng(39.319,-76.607),
 	  mapTypeId: 'terrain'
 	});
 
 	// Create a <script> tag and set the USGS URL as the source.
-	var script = document.createElement('script');
-	// This example uses a local copy of the GeoJSON stored at
-	// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-	script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-	document.getElementsByTagName('head')[0].appendChild(script);
+	// var script = document.createElement('script');
+	// // This example uses a local copy of the GeoJSON stored at
+	// // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+	// script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+	// document.getElementsByTagName('head')[0].appendChild(script);
+	loadJSON(function(jsn) { 
+
+		for (var i = 0; i < jsn.results.length; i++) {
+			// console.log(jsn.results[i].geometry.coordinates);
+			var coords = jsn.results[i].geometry.coordinates;
+			  var latLng = new google.maps.LatLng(coords[1],coords[0]);
+			  var marker = new google.maps.Marker({
+			    position: latLng,
+			    map: map
+			  });
+		}
+	});
 }
-
-loadJSON(function(jsn) { 
-
-	for (var i = 0; i < jsn.results.length; i++) {
-		// console.log(jsn.results[i].geometry.coordinates);
-		var coords = jsn.results[i].geometry.coordinates;
-		  var latLng = new google.maps.LatLng(coords[1],coords[0]);
-		  var marker = new google.maps.Marker({
-		    position: latLng,
-		    map: map
-		  });
-	}
-
-});
 
